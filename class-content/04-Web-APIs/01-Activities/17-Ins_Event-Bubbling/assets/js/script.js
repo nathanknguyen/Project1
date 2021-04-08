60e1e78ea729ec097bdde1cef2ca29f904cd73cc
John Young <john@codeandcreative.com>
2021-04-08 16:33:37 -0700

    Adds logging to week 4 activity 17

--- class-content/04-Web-APIs/01-Activities/17-Ins_Event-Bubbling/assets/js/script.js
+++ class-content/04-Web-APIs/01-Activities/17-Ins_Event-Bubbling/assets/js/script.js
@@ -4,22 +4,19 @@ var button = document.querySelector(".button");
 
 function changeBlue(event) {
   // event.stopPropagation();
-  event.currentTarget.setAttribute(
-    "style",
-    "background-color: blue"
-  );
+  console.log("changing blue!");
+  event.currentTarget.setAttribute("style", "background-color: blue");
 }
 
 function changePurple(event) {
   // event.stopPropagation();
-  event.currentTarget.setAttribute(
-    "style",
-    "background-color: #601A4A"
-  );
+  console.log("changing purple!");
+  event.currentTarget.setAttribute("style", "background-color: #601A4A");
 }
 
 function changeOrange(event) {
   // event.stopPropagation();
+  console.log("changing orange!");
   event.currentTarget.setAttribute(
     "style",
     "background-color: #EE442F; color: white;"
