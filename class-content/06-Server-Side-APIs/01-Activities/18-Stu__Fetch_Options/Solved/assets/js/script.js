3b31ad1e3a984634a67ff1aade6048c2b48abb24
John Young <john@codeandcreative.com>
2021-04-23 08:55:46 -0700

    Adds solutions to week 6 activities 16 and 18

---
+++ class-content/06-Server-Side-APIs/01-Activities/18-Stu__Fetch_Options/Solved/assets/js/script.js
@@ -0,0 +1,10 @@
+fetch("https://api.github.com/repos/nodejs/node/issues?per_page=5", {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
