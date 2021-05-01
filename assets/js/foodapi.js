caee21db7fe488919cc42953d971275372758d14
Ricky <rickyjoet@gmail.com>
2021-05-01 13:58:39 -0700

    function consolelog restaurant

--- assets/js/foodapi.js
+++ assets/js/foodapi.js
@@ -6,8 +6,38 @@ fetch('https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/11211',{
         "useQueryString": true
       }
 })
-  .then(response => response.json())
-  .then(data => console.log(data));
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(data){
+      console.log(data.result.data)
+      var restaurants = data.result.data
+        for (var i = 0; i < restaurants.length; i++) {
+            console.log(restaurants[i].address.city)
+            console.log(restaurants[i].address.formatted)
+            console.log(restaurants[i].hours)
+            console.log(restaurants[i].restaurant_name)
+            console.log(restaurants[i].restaurant_phone)
+            console.log(restaurants[i].cuisines)
+            console.log(restaurants[i].price_range)
+            console.log(restaurants[i].geo)
+    // var restAddress = document.createElement('tag');
+    // var restHours = document.createElement('tag');
+    // var resthours = document.createElement('tag');
+    // userName.textContent = data[i].user.login;
+    // issueTitle.textContent = data[i].title;
+    // issueContainer.append(userName);
+    // issueContainer.append(issueTitle);
+  }  
+  });
 
-  var user;
-    fetch(url).then
+
+
+  
+
+
+    // data.checkRestaurant = function (){
+    //     return data = document.getElementById ('');
+    // };
+
+    
