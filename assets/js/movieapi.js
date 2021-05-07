814aa339522e0d99cb8cd85649483b87454e8f93
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-05-06 19:31:37 -0700

    getting cinema data

--- assets/js/movieapi.js
+++ assets/js/movieapi.js
@@ -1,9 +1,13 @@
 fetch("https://api.internationalshowtimes.com/v4/cinemas/?location=47.60,-122.33&distance=100&apikey=5ambsfw2ewzFfiYpWQPQct4zgOv3khs8")
 .then(response => response.json())
 	.then(data => {
-    console.log(data.cinemas[0].location.lon)
-    console.log(data.cinemas[0].location.lat)
-    console.log(data.cinemas[0].name)
+    for (var i = 0; i < data.cinemas.length; i++) {
+      console.log(data.cinemas[i].location.lon)
+      console.log(data.cinemas[i].location.lat)
+      console.log(data.cinemas[i].name)
+
+    }
   })
 	.catch(err => console.error(err));
+  
 
