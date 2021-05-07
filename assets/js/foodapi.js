488c8f9dfbd0b8d613102100a7b53c7e8be791aa
Ricky <rickyjoet@gmail.com>
2021-05-06 20:51:35 -0700

    added brewery api

--- assets/js/foodapi.js
+++ assets/js/foodapi.js
@@ -1,35 +1,88 @@
-fetch('https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/98188',{
-    headers: {
-        'Content-Type': 'application/json',
-        "x-rapidapi-key": "73c4b148d4mshd043ad4720d8a11p1cc295jsn4cdf4a37dc05",
-        "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-        "useQueryString": true
-      }
-})
+document.querySelector("#zipcode-btn").addEventListener("click", function(event){
+  console.log("click")
+  var query = "https://api.openbrewerydb.org/breweries?by_postal="
+  var zipcodebox = document.querySelector("#zipcode-input").value
+  console.log(zipcodebox)
+  query = query + zipcodebox
+  console.log(query)
+  
+  fetch(query)
   .then(function(response){
       return response.json();
   })
   .then(function(data){
-    console.log(data.result.data)
-    var restaurants = data.result.data
-    for (var i = 0; i < restaurants.length; i++) { 
+    console.log(data)
+   // var restaurants = data.result.data
+   
+    for (var i = 0; i < data.length; i++) { 
+      if(data[i].latitude === null){
+        continue;
+      }
+      console.log(data[i].latitude)
+      console.log(data[i].longitude)
+      console.log(data[i].name)
+      console.log(data[i].street)
+      console.log(data[i].website_url)
+
+
+      // var restaurant = document.getElementById('restaurant');
+      // var restaurantName = document.createElement('p');
+      // var restaurantPhone = document.createElement('p');
+      // var restaurantAddress = document.createElement('p')
+      
+      // restaurant.appendChild(restaurantName);
+      // restaurant.appendChild(restaurantPhone);
+      // restaurant.appendChild(restaurantAddress)
+      
+      // restaurantName.textContent = restaurants[i].restaurant_name;
+      // restaurantPhone.textContent = restaurants[i].restaurant_phone;
+      // restaurantAddress.textContent = restaurants[i].address.formatted;
     
-      var restaurant = document.getElementById('restaurant');
-      var restaurantName = document.createElement('p');
-      var restaurantPhone = document.createElement('p');
-      var restaurantAddress = document.createElement('p')
-      
-      restaurant.appendChild(restaurantName);
-      restaurant.appendChild(restaurantPhone);
-      restaurant.appendChild(restaurantAddress)
-      
-      restaurantName.textContent = restaurants[i].restaurant_name;
-      restaurantPhone.textContent = restaurants[i].restaurant_phone;
-      restaurantAddress.textContent = restaurants[i].address.formatted;
-      
       
     } 
     });
+
+  })
+
+
+
+// API Brewries
+// text box and button function search by zipcode
+document.querySelector("#zipcode-btn").addEventListener("click", function(event){
+  console.log("click")
+  var query = "https://api.openbrewerydb.org/breweries?by_postal="
+  var zipcodebox = document.querySelector("#zipcode-input").value
+  query = query + zipcodebox
+  console.log(query)
+})
+// //text box and button function search by state
+// document.querySelector("#zipcode-btn").addEventListener("click", function(event){
+//   console.log("click")
+//   var queryState ="https://api.openbrewerydb.org/breweries?by_state=washington"
+//   var zipcodebox = document.querySelector("#zipcode-input").value
+//   queryState = queryState + zipcodebox
+//   console.log(queryState)
+// })
+// //text box and button function search by city
+// document.querySelector("#zipcode-btn").addEventListener("click", function(event){
+//   console.log("click")
+//   var queryCity ="https://api.openbrewerydb.org/breweries?by_state=washington"
+//   var zipcodebox = document.querySelector("#zipcode-input").value
+//   queryCity = queryCity + zipcodebox
+//   console.log(queryCity)
+
+
+// fetch('https://api.openbrewerydb.org/breweries?by_state=washington')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+
+
+//  fetch('https://api.openbrewerydb.org/breweries?by_city=seattle')
+//   .then(response => response.json())
+  // .then(data => console.log(data));
+  
     
     //   }
     //     console.log(restaurants[i].address.city)
@@ -61,5 +114,3 @@ fetch('https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/98188',{
     // data.checkRestaurant = function (){
       //     return data = document.getElementById ('');
       // };
-      
-      
